44652560da1d9372a82bdf4b55271dbf2bc3c556
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-22 16:28:07 -0500

    I coded the function with the use of library random

--- Random_Number.py
+++ Random_Number.py
@@ -3,4 +3,8 @@
 
 #Calling the function multiple times should (usually) return different numbers.
 
-#E.g, calling random_number some times might firsr return 42, then 63, then 1.
+#E.g, calling random_number some times might firsr return 42, then 63, then 1. 
+import random
+
+def random_number():
+    return random.randint(1, 100) 
