99e7d234645a030726f81688b0ada7fa9d0bf2c9
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-22 16:15:36 -0500

    First committ

---
+++ Random_Number.py
@@ -0,0 +1,6 @@
+#   Define a function, 'random_number', that takes no parameters. 
+#   The function must generate a random integer between 1 and 100, both inclusive and return it.
+
+#Calling the function multiple times should (usually) return different numbers.
+
+#E.g, calling random_number some times might firsr return 42, then 63, then 1.
